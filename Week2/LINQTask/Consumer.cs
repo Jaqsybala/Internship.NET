37e0e36af0ab4b6437355bcb93ab82af06a91e79
Kassimov Aziz <aziz.kassimov@gmail.com>
2022-04-07 23:52:11 +0600

    Add LINQ task

---
+++ Week2/LINQTask/Consumer.cs
@@ -0,0 +1,16 @@
+﻿namespace Week2.LINQTask
+{
+    public class Consumer
+    {
+        public int ConsumerCode { get; set; }
+        public int YearOfBirth { get; set; }
+        public string Address { get; set; }
+
+        public Consumer(int consumerCode, int yearOfBirth, string address)
+        { 
+            ConsumerCode = consumerCode;
+            YearOfBirth = yearOfBirth;
+            Address = address;
+        }
+    }
+}
---
+++ Week2/LINQTask/Goods.cs
@@ -0,0 +1,16 @@
+﻿namespace Week2.LINQTask
+{
+    public class Goods
+    {
+        public string ArticleNumber { get; set; }
+        public string Category { get; set; }
+        public string CountryOfOrigin { get; set; }
+
+        public Goods(string articleNumber, string category, string countryOfOrigin)
+        {
+            ArticleNumber = articleNumber;
+            Category = category;
+            CountryOfOrigin = countryOfOrigin;
+        }
+    }
+}
